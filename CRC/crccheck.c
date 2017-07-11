a18d7e0c156bed280568ed489ff36affb9a12a01
AureCH <arieh94@gmail.com>
2017-07-11 19:18:53 -0400

    Added Library/Main

--- CRC/crccheck.c
+++ CRC/crccheck.c
@@ -2,3 +2,23 @@
 //	CIS 3360
 //	Assignment 2 - CRC
 //	7-11-2017
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+#define CRCpolynomial 1010000001010011
+
+
+
+int main(int argc, char *argv[])
+{
+	int i =0, j, length;
+	char *crcpoly;
+	strcpy(crcpoly, CRCpolynomial);
+	printf("%s", crcpoly);
+	
+	FILE* ifp = fopen()
+}
