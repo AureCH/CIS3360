bfcdcf9cb38a7170bd2675b829c261e5ce263c56
Christian Helms <arieh94@gmail.com>
2017-07-11 20:29:48 -0400

    Adding global define, and some logic to handle polynomial size

--- CRC/crccheck.c
+++ CRC/crccheck.c
@@ -9,16 +9,35 @@
 #include <ctype.h>
 #include <string.h>
 
-#define CRCpolynomial 1010000001010011
+#define CRCpolynomial "1010000001010011"
 
 
 
-int main(int argc, char *argv[])
+int main()//(int argc, char *argv[])
 {
-	int i =0, j, length;
-	char *crcpoly;
+	int i =0, j, length,crclength;
+		printf("%s", CRCpolynomial);
+		fflush(stdout);
+	crclength = strlen(CRCpolynomial);
+	int *crcpoly[crclength];
+	printf("\n%d\n", crclength);
+	for(i=0; i< crclength; i++)
+		{
+			if(i == 0)
+				printf("\n%d\n", __LINE__);
+			crcpoly = atoi(CRCpolynomial);
+//			crcpoly[crclength-i-1] = 1;//CRCpolynomial[i];
+		} 
+	printf("\nhere %d\n", __LINE__);
+	fflush(stdout);
+//	printf("\n%d\n", *crcpoly);
+
+/*
+	printf("hi\n");
+	fflush(stdout);
 	strcpy(crcpoly, CRCpolynomial);
 	printf("%s", crcpoly);
-	
-	FILE* ifp = fopen()
+	fflush(stdout);
+*/	
+//	FILE* ifp = fopen()
 }
